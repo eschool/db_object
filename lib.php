6b5fcddef2edf3c2081403337a1ee8aae47e71e4
John Colvin <john.colvin@eschoolconsultants.com>
2011-03-29 16:13:47 -0400

    Brought over more needed DB functions from SIS code.

--- lib.php
+++ lib.php
@@ -602,3 +602,100 @@ function add_or_to_sql($sql, $where_sql)
    else
       return $sql . " OR $where_sql";
 }
+
+function get_sql_where_string($field_name, $value, $operator='=')
+{
+    $field_name = trim($field_name);
+    $value = trim($value);
+    $operator = trim($operator);
+
+    $sql_string = mysql_real_escape_string($field_name) . " $operator ";
+
+    if (false !== strpos($value, "'", 0) && false !== strpos($value, "'", strlen($value) - 1)) {
+        //  Value is already quoted
+        $sql_string .= $value;
+    } else {
+        $sql_string .= "'" . $value . "'";
+    }
+
+    return $sql_string;
+}
+
+/**
+ * This function takes an array and puts into the following SQL format:
+ * ex.: field_name IN ('value1', 'value2', 'value3')
+ *
+ * By default, the sql string will look return an IN query. To negate, set the $negative
+ * flag to TRUE.
+ *
+ * Paramters:
+ *  $data         = An array that contains various values that you need to check
+ *                to see if the field is equal to.
+ *  $field_name   = The name of the database field to compare the values
+ *                in the $data array to.
+ * @param bool $negative
+ */
+function get_sql_in_string( $data, $field_name, $negative=FALSE )
+{
+    /**
+     * WARNING: passing empty $data will cause a string like:
+     * student_id IN ('') to be returned.
+     */
+
+    if (!is_array($data))
+       $data = array($data);
+
+    $in_array = array();
+    foreach ($data as $element) {
+        $in_array[] = "'" . mysql_real_escape_string($element) . "'";
+    }
+    $in_string = implode(',', $in_array);
+
+    // Making sure an empty string doesn't cause problems
+    if (strlen(trim($in_string)) == 0)
+        $in_string = "''";
+
+    if ( $negative === FALSE )
+    {
+        $sql_string = '' . mysql_real_escape_string($field_name) . ' IN (' . $in_string . ')';
+    }
+    else
+    {
+        $sql_string = mysql_real_escape_string( $field_name ) . ' NOT IN (' . $in_string . ')';
+    }
+
+    return $sql_string;
+}
+
+/**
+ * Simply calls mysql_query() and returns the resource.
+ * The only reason I have abstracted it is to log the query in $GLOBALS['query_check'].
+ *
+ * @param string $query_string
+ * @param boolean $debug
+ * @return resource OR boolean
+ * @author Bryce Thornton
+ */
+function query_resource($query_string, $debug = false)
+{
+    if (is_string($query_string)) {
+        if ($debug) {
+          debug($query_string);
+        }
+
+        $result = mysql_query($query_string);
+
+        // increment query check variable for reporting purposes
+        // but first, make sure it's set
+        if (!isset($GLOBALS['query_check']['query_resource()']))
+            $GLOBALS['query_check']['query_resource()'] = 0;
+
+        $GLOBALS['query_check']['query_resource()']++;
+
+        if (is_resource($result)) {
+            return $result;
+        } else {
+            return FALSE;
+        }
+    }
+}
